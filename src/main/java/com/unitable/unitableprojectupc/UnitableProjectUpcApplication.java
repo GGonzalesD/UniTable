f27cd7d2a2310deb341e5ca93fa7e95b16a15884
GGonzalesD <grober_gonzales@hotmail.com>
2021-09-21 19:52:06 -0500

    Creando proyecto UniTable

---
+++ src/main/java/com/unitable/unitableprojectupc/UnitableProjectUpcApplication.java
@@ -0,0 +1,13 @@
+package com.unitable.unitableprojectupc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitableProjectUpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitableProjectUpcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unitable/unitableprojectupc/UnitableProjectUpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unitable.unitableprojectupc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitableProjectUpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
